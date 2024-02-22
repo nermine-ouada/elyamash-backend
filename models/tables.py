5cd272304703936ca38b4c53b2a178c94b358d4e
nermine.ouada <nermine.ouada@gmail.com>
2024-02-22 23:09:35 +0100

    added updated at and disabled columns

--- models/tables.py
+++ models/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, ForeignKey, DateTime, Integer
+from sqlalchemy import Column, String, ForeignKey, DateTime, Integer,Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
@@ -12,9 +12,13 @@ class User(Base):
     id = Column(String, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
     hashed_password = Column(String)
     role = Column(String)
+    age = Column(Integer, nullable=True)
+    full_name = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+    disabled = Column(Boolean, default=False)
 
     # Relationship to images and votes
     images = relationship("Image", back_populates="user")
@@ -29,14 +33,16 @@ class Image(Base):
     image_desc = Column(String)
     image_path = Column(String, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
     rating = Column(Integer)
+    disabled = Column(Boolean, default=False)
 
     # Foreign Key relationship with User
     user_id = Column(String, ForeignKey("user.id"))
 
     # Relationship to users
     user = relationship("User", back_populates="images")
-
+ 
 
 class Vote(Base):
     __tablename__ = "vote"
@@ -44,12 +50,13 @@ class Vote(Base):
     voter_id = Column(String, ForeignKey("user.id"))
     image1_id = Column(String, ForeignKey("image.id"))
     image2_id = Column(String, ForeignKey("image.id"))
-    score_image1 = Column(Integer)  
-    score_image2 = Column(Integer)  
+    score_image1 = Column(Integer)
+    score_image2 = Column(Integer)
     voted_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+    disabled = Column(Boolean, default=False)
 
     # Relationship to images and users
     image1 = relationship("Image", foreign_keys=[image1_id])
-    image2 = relationship("Image", foreign_keys=[image2_id]) 
+    image2 = relationship("Image", foreign_keys=[image2_id])
     user = relationship("User", back_populates="votes")
-  
